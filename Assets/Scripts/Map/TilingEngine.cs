b18dde298ce5db69c5568238c37e3dc1d23fed8e
Nicolas Dupont <nicolas@akeneo.com>
2016-11-05 18:04:38 +0100

    Add basic player and camera controller

--- Assets/Scripts/Map/TilingEngine.cs
+++ Assets/Scripts/Map/TilingEngine.cs
@@ -6,7 +6,6 @@ using Lean;
 /**
  * Tiling Engine inspired by the following tutorial https://gamedevacademy.org/how-to-script-a-2d-tile-map-in-unity3d/
  */
-//[ExecuteInEditMode]
 public class TilingEngine : MonoBehaviour
 {
     public string tilesetSpriteName;
@@ -49,8 +48,8 @@ public class TilingEngine : MonoBehaviour
         SmoothTilesGenerator smoothGenerator = new SmoothTilesGenerator();
         tiles = smoothGenerator.Generate(tiles, width, height);
 
-        //FinalTilesGenerator finalGenerator = new FinalTilesGenerator();
-        //tiles = finalGenerator.Generate(tiles, width, height);
+        FinalTilesGenerator finalGenerator = new FinalTilesGenerator();
+        tiles = finalGenerator.Generate(tiles, width, height);
 
         var index = 0;
         for (var y = 0; y < height; y++) {
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    //public Text countText;
+    //public Text winText;
+
+    private Rigidbody2D rb2d;
+    //private int count;
+
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        /*count = 0;
+        setCountText();
+        winText.text = "";*/
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+/*
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp")) {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            setCountText();
+        }
+    }
+
+    void setCountText() {
+        countText.text = "Count : " + count.ToString();
+        if (count >= 8) {
+            winText.text = "You Win!";
+        }
+    }*/
+}
